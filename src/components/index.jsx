7796e994e05e6faaac32ffbc820b19404d305626
Oktamov Shokhjahon <oktamovshokhjahon@gmail.com>
2024-12-19 17:04:26 +0500

    takliflar

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Chances from "../pages/chances";
 import EducationalCarousel from "../pages/Carousel";
+import Compensation from "../pages/Compensation";
 
 export default class index extends Component {
   render() {
@@ -9,6 +10,7 @@ export default class index extends Component {
         <div className="container">
           <EducationalCarousel />
           <Chances />
+          <Compensation />
         </div>
       </div>
     );
