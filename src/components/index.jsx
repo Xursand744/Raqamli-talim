378cd2fc532e023c167984de06b252a69d5f738c
Oktamov Shokhjahon <oktamovshokhjahon@gmail.com>
2024-12-19 16:41:19 +0500

    carousel

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import Chances from "../pages/chances";
+import EducationalCarousel from "../pages/Carousel";
 
 export default class index extends Component {
   render() {
     return (
       <div>
         <div className="container">
+          <EducationalCarousel />
           <Chances />
         </div>
       </div>
