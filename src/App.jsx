ce6699d2af15be1456561efda5653d8eb4484128
Xursand <xursandmadaminov1995@gmail.com>
2024-12-20 22:31:10 +0500

    about

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
           element: <HomePage />,
         },
         {
-          path: "about",
+          path: "/about",
           element: <About />,
         },
         {
