dae4978b7398e27e4f37e733cffd84acc2366a77
Shohjahon <oktamovshokhjahon@gmail.com>
2024-12-23 20:00:39 +0500

    course end

--- src/pages/Tips.jsx
+++ src/pages/Tips.jsx
@@ -6,14 +6,6 @@ function Tips() {
   return (
     <div className="max-w-[1300px] w-full my-0 mx-auto mt-[100px] mb-[100px]">
       <TipsFilter />
-
-      {/* <button
-        onClick={() => setIsModalOpen(true)}
-        className="mt-10 mx-auto block bg-blue-500 text-white px-6 py-2 rounded"
-      >
-        Open Modal
-      </button>
-      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} /> */}
     </div>
   );
 }
