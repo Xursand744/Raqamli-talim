0423adcdc5651a933ed8300e3a13fe624596933e
Oktamov Shokhjahon <oktamovshokhjahon@gmail.com>
2025-01-07 16:01:16 +0500

    university

--- src/components/PriceRange.jsx
+++ src/components/PriceRange.jsx
@@ -194,8 +194,8 @@ export default function PriceRange({ priceRange, setPriceRange }) {
             <div
               className="absolute h-1 bg-blue-500 rounded-lg"
               style={{
-                left: `${priceRange / 50000}%`,
-                right: `${100 - priceRange / 50000}%`,
+                left: `${priceRange.min / 50000}%`,
+                right: `${100 - priceRange.max / 50000}%`,
               }}
             />
           </div>
@@ -204,7 +204,7 @@ export default function PriceRange({ priceRange, setPriceRange }) {
             name="min"
             min="0"
             max="5000000"
-            value={priceRange}
+            value={priceRange.min}
             onChange={handleRangeChange}
             className="absolute w-full h-1 -mt-1 bg-transparent appearance-none pointer-events-none [&::-webkit-slider-thumb]:pointer-events-auto [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-blue-500 [&::-webkit-slider-thumb]:cursor-pointer [&::-moz-range-thumb]:pointer-events-auto [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-blue-500 [&::-moz-range-thumb]:cursor-pointer [&::-ms-thumb]:pointer-events-auto [&::-ms-thumb]:w-4 [&::-ms-thumb]:h-4 [&::-ms-thumb]:appearance-none [&::-ms-thumb]:rounded-full [&::-ms-thumb]:bg-blue-500 [&::-ms-thumb]:cursor-pointer"
           />
@@ -213,7 +213,7 @@ export default function PriceRange({ priceRange, setPriceRange }) {
             name="max"
             min="0"
             max="5000000"
-            value={priceRange}
+            value={priceRange.max}
             onChange={handleRangeChange}
             className="absolute w-full h-1 -mt-1 bg-transparent appearance-none pointer-events-none [&::-webkit-slider-thumb]:pointer-events-auto [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-blue-500 [&::-webkit-slider-thumb]:cursor-pointer [&::-moz-range-thumb]:pointer-events-auto [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-blue-500 [&::-moz-range-thumb]:cursor-pointer [&::-ms-thumb]:pointer-events-auto [&::-ms-thumb]:w-4 [&::-ms-thumb]:h-4 [&::-ms-thumb]:appearance-none [&::-ms-thumb]:rounded-full [&::-ms-thumb]:bg-blue-500 [&::-ms-thumb]:cursor-pointer"
           />
@@ -221,10 +221,10 @@ export default function PriceRange({ priceRange, setPriceRange }) {
         <div className="flex gap-4 items-center">
           <input
             type="text"
-            value={formatPrice(priceRange)}
+            value={formatPrice(priceRange.min)}
             onChange={(e) => {
               const value = parseInt(e.target.value.replace(/[^0-9]/g, ""));
-              if (!isNaN(value) && value >= 0 && value <= priceRange) {
+              if (!isNaN(value) && value >= 0 && value <= priceRange.max) {
                 setPriceRange((prev) => ({ ...prev, min: value }));
               }
             }}
@@ -232,10 +232,14 @@ export default function PriceRange({ priceRange, setPriceRange }) {
           />
           <input
             type="text"
-            value={formatPrice(priceRange)}
+            value={formatPrice(priceRange.max)}
             onChange={(e) => {
               const value = parseInt(e.target.value.replace(/[^0-9]/g, ""));
-              if (!isNaN(value) && value >= priceRange && value <= 5000000) {
+              if (
+                !isNaN(value) &&
+                value >= priceRange.min &&
+                value <= 5000000
+              ) {
                 setPriceRange((prev) => ({ ...prev, max: value }));
               }
             }}
