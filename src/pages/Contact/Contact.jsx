cc25946e7bd1d0eeb10a0083deaa56197a80269e
G'ulomjon <legioncpm@gmail.com>
2025-02-18 22:35:43 +0500

    contact

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,15 @@
-function Contact() {
-  return <div>Contact</div>;
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <main>
+        <section className='py-[180px]'>
+            <div className='flex justify-center'><h1 className='pb-[40px] font-bold text-[44px] leading-[64px] text-[#222222]'>Biz bilan bog‘laning</h1></div>
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default Contact;
