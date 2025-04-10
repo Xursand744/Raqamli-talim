2eda4fc3a20ecfad1289493d94865a1446789cb3
sarvarkhursandov <urbandriveuz@gmail.com>
2025-04-10 10:20:55 +0500

    success story added

--- src/components/SuccessHistoryItem.jsx
+++ src/components/SuccessHistoryItem.jsx
@@ -1,35 +1,26 @@
 function SuccessHistoryItem({ studyCenter, fullname, desc, tags, image }) {
   return (
-    <div>
-      <div className="max-w-[384px] max-h-[200px]">
+    <div className="max-w-[384px] rounded-lg overflow-hidden shadow-lg">
+      <div className="h-[200px]">
         <img
           src={image}
-          className="w-full h-full"
-          style={{ borderTopRightRadius: "10px", borderTopLeftRadius: "10px" }}
+          className="w-full h-full object-cover"
           alt=""
         />
       </div>
 
-      <div
-        style={{
-          borderBottomLeftRadius: "10px",
-          borderBottomRightRadius: "10px",
-          border: "1px solid #E0E0E0",
-          maxWidth: "384px",
-          padding: "16px",
-        }}
-      >
+      <div className="border-t border-gray-200 p-4 bg-white">
         {/* <p className="text-[14px]">{studyCenter}</p> */}
-        <h2 className="text-[#222] text-[20px] font-medium mt-[8px] mb-[12px]">
+        <h2 className="text-[#222] text-[20px] font-medium mt-2 mb-3">
           {fullname}
         </h2>
-        <p className="mb-[24px]">{desc}</p>
-        <div className="flex gap-[12px]">
+        <p className="mb-4 text-gray-700">{desc}</p>
+        <div className="flex gap-3">
           {/* {tags.map((tag) => {
             return (
               <span
                 key={tag}
-                className="bg-[#F0F3FF] rounded-[22px] py-[4px] px-[16px]"
+                className="bg-[#F0F3FF] rounded-full py-1 px-4 text-sm"
               >
                 <span className="text-[#2675EB]">#</span> {tag}
               </span>
