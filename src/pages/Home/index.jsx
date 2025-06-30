6c4602788dcd7168bfd324a38c401e6d9259987b
sarvarkhursandov <urbandriveuz@gmail.com>
2025-06-30 17:52:14 +0500

    added main page seo texts

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,28 +9,46 @@ import SuccessHistory from "./SuccessHistory";
 // import Join from "./Join";
 import Carousel from "../Carousel";
 // import JobAnalytics from "./JobAnalytics";
+import { Helmet } from "react-helmet-async";
+import { useTranslation } from "react-i18next";
 
 export default function HomePage() {
+  const { t } = useTranslation("global");
   return (
-    <div>
-      <Carousel />
-      <div className="w-full">
-        {/* <JobAnalytics /> */}
-      </div>
-      <div className="max-w-[1440px] mx-auto mt-[100px]">
-        {/* <Chances /> */}
-      </div>
-      <Plan />
-      <Compensation />
-      <Counts />
-      <Darection />
+    <>
+      <Helmet>
+        <title>{t('home.seo.title')}</title>
+        <meta name="description" content={t('home.seo.description')} />
+        <meta name="keywords" content={t('home.seo.keywords')} />
+        {/* Open Graph */}
+        <meta property="og:title" content={t('home.seo.title')} />
+        <meta property="og:description" content={t('home.seo.description')} />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Raqamli Ta'lim" />
+        {/* Twitter */}
+        <meta name="twitter:title" content={t('home.seo.title')} />
+        <meta name="twitter:description" content={t('home.seo.description')} />
+      </Helmet>
+      <div>
+        <Carousel />
+        <div className="w-full">
+          {/* <JobAnalytics /> */}
+        </div>
+        <div className="max-w-[1440px] mx-auto mt-[100px]">
+          {/* <Chances /> */}
+        </div>
+        <Plan />
+        <Compensation />
+        <Counts />
+        <Darection />
 
-      <div className="container">
-        <SuccessHistory />
-      </div>
+        <div className="container">
+          <SuccessHistory />
+        </div>
 
-      <School />
-      {/* <Join /> */}
-    </div>
+        <School />
+        {/* <Join /> */}
+      </div>
+    </>
   );
 }
